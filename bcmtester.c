1f3e4a1e7dd31d674a0a03072d2ad63552a34bcc
Connor Teal <teal2c@uw.edu>
2021-01-23 19:42:51 -0800

    Starting the project for BME280 sensor reading over I2C

---
+++ bcmtester.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <bcm2835.h>
+
+
+unsigned short TEMP_TRIM1;
+short TEMP_TRIM2;
+short TEMP_TRIM3;
+
+unsigned short PRESSURE_TRIM1;
+short PRESSURE_TRIM2;
+short PRESSURE_TRIM3;
+short PRESSURE_TRIM4;
+short PRESSURE_TRIM6;
+short PRESSURE_TRIM7;
+short PRESSURE_TRIM8;
+short PRESSURE_TRIM9;
+
+unsigned char HUMIDITY_TRIM1;
+short HUMIDITY_TRIM2;
+unsigned char HUMIDITY_TRIM3;
+short HUMIDITY_TRIM4;
+short HUMIDITY_TRIM5;
+
+void get_temp_trim_one();
+ 
+int main(void){
+	int init_ok, i2c_ok;
+	char buf[]= {0xFA};
+	init_ok = bcm2835_init();
+	if (init_ok == 0){
+		printf("bcm2835_init() failed\n");
+		exit(EXIT_FAILURE);
+	}
+	bcm2835_i2c_setClockDivider(BCM2835_I2C_CLOCK_DIVIDER_2500);
+	i2c_ok = bcm2835_i2c_begin();
+	if (i2c_ok == 1){
+		printf("i2c_begin successful\n");
+	}
+	//bcm2835_i2c_setSlaveAddress(0x76);
+	//bcm2835_i2c_write_read_rs(buf,1,buf,3);
+	//bcm2835_i2c_write(buf,1);
+	//bcm2835_i2c_read(buf,3);
+	//printf("User Reg1 = %x\n",buf[0]);
+	//printf("User Reg2 = %x\n",buf[1]);
+	//printf("User Reg3 = %x\n",buf[2]);
+	
+	get_temp_trim_one();
+	printf("Temp_T_1 = %x\n", TEMP_TRIM1);
+	
+	char buf2[] = {0xE0,0xB6};
+	bcm2835_i2c_write(buf2,2);
+	bcm2835_i2c_end();
+	
+}
+void get_temp_trim_one(void){
+	char buf[] = {0x88};
+	bcm2835_i2c_write_read_rs(buf,1,buf,2);
+	TEMP_TRIM1 = (buf[1]<<8)|(buf[0]);	
+	
+}
